0e8caaf16e040413d142d38e224ea2ca95c1b5ad
F1rdavs9644 <begmatovsamandar726@gmail.com>
2025-03-05 20:17:23 +0500

    project routes are connected

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+    // return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/job-details', function () {
+    return view('job-details');
+});
+
+Route::get('/job-listings', function () {
+    return view('job-listings');
+});
